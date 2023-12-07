f2fb34f582eeb214eeff9bf88508ca0ae7f3695c
Donna6355 <donnajun1214@gmail.com>
2023-12-07 17:37:16 +0900

    [study] more on super keyword

--- lib/inheritance_sample.dart
+++ lib/inheritance_sample.dart
@@ -1,31 +1,44 @@
-
 class Vehicle {
-  Vehicle(this.color);
+  Vehicle(this.color) {
+    print('Vehicle $color constructor is called');
+  }
 
   final String color;
   final String definition = 'Vehicle';
 }
 
 class Car extends Vehicle {
-  Car(String color) : super(color); //you need to inherit two var, one super the other override
+  Car(String color)
+      : super(
+            color); //you need to inherit two var, one super the other override
 
   @override
   String get definition => 'Car ${super.definition}';
 }
 
 class Hatch extends Car {
-  Hatch(String color) : super(color);
+  int price;
+  Hatch(int price)
+      : price = price, // sub class 단독 var가 있으면 먼저 해야됨!
+        super('red'); //Hatch constructor가 호출되면 => parent constructor가 호출된다옹
+}
+
+class BMW extends Car {
+  BMW(super.color); //BMW constructor의 argument가 바로 super.color로 할당됨!
 }
 
 main() {
   final hatch = Hatch('red');
+  final bmw = BMW('green');
+  print(hatch.color);
   print('Result: ${hatch is Vehicle}');
+  print(bmw.color);
+  print('Result: ${bmw is Vehicle}');
   // Output -> Result: true
 }
 
 // When class Car extends Vehicle, all properties, variables, functions implemented in class Vehicle will be available in class Car.
 // You use extends when you want to create a more specific version of a class.(copy the Super class all + addition)
-
 
 class Animal0 {}
 
@@ -56,11 +69,11 @@ class Duck0 extends Animal0 implements Swimmer0, Flyer0 {
 class Animal {}
 
 // behaviors
- mixin Flyer {
+mixin Flyer {
   void fly() => print('I can fly!');
 }
 
- mixin Swimmer {
+mixin Swimmer {
   void swim() => print('I can swim!');
 }
 
