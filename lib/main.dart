16327183a5f581616cc1244b11cb85254bd74285
Donna6355 <donnajun1214@gmail.com>
2023-12-14 10:05:57 +0900

    [study] filled, outlined, elevated button with material3

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,33 @@ import 'package:test/gorouter_sample.dart';
 void main() {
   runApp(
     ProviderScope(
-      child: 
-      MaterialApp.router(
+      child: MaterialApp.router(
+        theme: ThemeData(
+          colorSchemeSeed: Colors.white,
+          filledButtonTheme: FilledButtonThemeData(
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              ),
+            ),
+          ),
+          outlinedButtonTheme: OutlinedButtonThemeData(
+            style: ButtonStyle(
+              side: MaterialStateProperty.all(
+                const BorderSide(color: Colors.green),
+              ),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              ),
+            ),
+          ),
+        ),
         routerConfig: router,
       ),
-    ),  
+    ),
   );
 }
--- lib/riverpod_sample.dart
+++ lib/riverpod_sample.dart
@@ -1,42 +1,51 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 class RiverPodSmp extends ConsumerWidget {
-   RiverPodSmp({super.key});
-    final counterProvider = StateNotifierProvider((ref) => Counter());
+  RiverPodSmp({super.key});
+  final counterProvider = StateNotifierProvider((ref) => Counter());
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final count = ref.watch(counterProvider);
-    // ref.listen(counterProvider, (previous, next) { 
+    // ref.listen(counterProvider, (previous, next) {
     //   print('riverpod count :$previous, $next');
     // });
 
     return Scaffold(
-        appBar: AppBar(title: const Text('Example')),
-        body: Center(
-          child: Column(
-            children: [
-              Text('$count'),
-              ElevatedButton(
-                onPressed: () => context.go('/iamport',extra: 'test'), 
-                child: const Text('to Freezed Sample'),
-              ),
-            ],
-          ),
+      appBar: AppBar(title: const Text('Example')),
+      body: Center(
+        child: Column(
+          children: [
+            Text('$count'),
+            ElevatedButton(
+              onPressed: () => context.go('/iamport', extra: 'test'),
+              child: const Text('to Freezed Sample'),
+            ),
+            OutlinedButton(
+              onPressed: () => context.go('/mediaquery'),
+              child: const Text('go Media Query Sample'),
+            ),
+            FilledButton(
+              onPressed: () => context.go('/dial'),
+              child: const Text('go dial Sample'),
+            ),
+          ],
         ),
-        floatingActionButton: ElevatedButton(onPressed: ref.watch(counterProvider.notifier).increment, child: const Text('increment'),),
-      );
+      ),
+      floatingActionButton: ElevatedButton(
+        onPressed: ref.watch(counterProvider.notifier).increment,
+        child: const Text('increment'),
+      ),
+    );
   }
 }
 
-class Counter extends StateNotifier<int>{
-  Counter():super(0);
+class Counter extends StateNotifier<int> {
+  Counter() : super(0);
 
   void increment() => state++;
-
 }
 
 /*
