5b4e2177d3e353ecbb36d1d488e1839d98ef6a93
Donna <donnajun1214@gmail.com>
2024-05-08 09:53:28 +0900

    [add] riveasset button tutorial

--- lib/rive_sample.dart
+++ lib/rive_sample.dart
@@ -53,16 +53,14 @@ class _BirdState extends State<Bird> {
             'Toggle을 하면 춤을 추고, \n버튼을 클릭하면 예정된 움직임 한번 실행되는 버전',
             textAlign: TextAlign.center,
           ),
-          Expanded(
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: RiveAnimation.asset(
-                'assets/bird.riv',
-                onInit: _onInit,
-                fit: BoxFit.fitWidth,
-                alignment: Alignment.center,
-              ),
+          SizedBox(
+            height: 200,
+            width: 200,
+            child: RiveAnimation.asset(
+              'assets/bird.riv',
+              onInit: _onInit,
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.center,
             ),
           ),
           Row(
@@ -84,11 +82,65 @@ class _BirdState extends State<Bird> {
                 child: const Text('look up'),
               ),
             ],
-          )
+          ),
+          LilGuyTwo(),
         ],
       ),
     );
   }
 }
+
+/// board 안에 있는 Button을 클릭하여 동작 시키는 버전
+class LilGuyTwo extends StatefulWidget {
+  const LilGuyTwo({super.key});
+
+  @override
+  State<LilGuyTwo> createState() => _LilGuyTwoState();
+}
+
+class _LilGuyTwoState extends State<LilGuyTwo> {
+  late StateMachineController _stmController;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _onInit(Artboard art) {
+    _stmController = StateMachineController.fromArtboard(art, 'State Machine 1')
+        as StateMachineController;
+
+    _stmController.isActive = true;
+
+    _stmController.addEventListener(onRiveEvent);
+
+    art.addController(_stmController);
+  }
+
+  void onRiveEvent(RiveEvent event) {
+    event.properties['Number 1' as double];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 100),
+        const Text('RiveAsset 안에 있는 Button을 클릭하여 동작 시키는 버전'),
+        SizedBox(
+          height: 200,
+          width: 200,
+          child: RiveAnimation.asset(
+            'assets/lil_guy.riv',
+            onInit: _onInit,
+            fit: BoxFit.fitWidth,
+            alignment: Alignment.center,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 
 //https://medium.com/@moo_min/rive%EB%A5%BC-flutter%EC%97%90%EC%84%9C-%EC%82%AC%EC%9A%A9%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95-1533ccbfc7ac
