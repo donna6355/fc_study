5bde9947d4af2b41cf35021526e42d4478bfe4ff
Donna <donnajun1214@gmail.com>
2024-05-10 10:30:24 +0900

    [add] try implement in freezed and failed

---
+++ lib/model/test_ui_state_model.dart
@@ -0,0 +1,24 @@
+// import 'package:freezed_annotation/freezed_annotation.dart';
+
+// part 'test_ui_state_model.freezed.dart';
+
+// enum Status {
+//   loading,
+//   success,
+//   failure,
+// }
+
+// abstract class DataStatus {
+//   Status get status;
+// }
+
+// @freezed
+// class TestUiState with _$TestUiState {
+//   factory TestUiState({
+//     required int id,
+//     required String name,
+//     required int age,
+//   }) = _TestUiState;
+//   @Implements<DataStatus>()
+// }
+// // dart run build_runner build
