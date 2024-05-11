c3bdfda2b829f091431b29e134a240b59eaa5d40
Donna <donnajun1214@gmail.com>
2024-05-11 20:28:00 +0900

    [fix] bloc with enum

--- lib/counter/bloc_sample.dart
+++ lib/counter/bloc_sample.dart
@@ -58,21 +58,17 @@ class CounterPage extends StatelessWidget {
 enum CounterEvent { increment, decrement }
 
 class CounterBloc extends Bloc<CounterEvent, int> {
-  CounterBloc() : super(0);
-
-  void increment() => emit(state + 1);
-  void decrement() => emit(state - 1);
-
-  @override
-  Stream<int> mapEventToState(CounterEvent event) async* {
-    switch (event) {
-      case CounterEvent.increment:
-        yield state + 1;
-        break;
-      case CounterEvent.decrement:
-        yield state - 1;
-        break;
-    }
+  CounterBloc() : super(0) {
+    on((event, emit) {
+      switch (event) {
+        case CounterEvent.increment:
+          emit(state + 1);
+          break;
+        case CounterEvent.decrement:
+          emit(state - 1);
+          break;
+      }
+    });
   }
 }
 
