70d2d34e01740cc7f86926a48704897c07ed13ca
Donna <donnajun1214@gmail.com>
2024-05-22 22:00:50 +0900

    [add] basic way to create module

---
+++ name_of_module/lib/name_of_module.dart
@@ -0,0 +1,9 @@
+library name_of_module;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
+//flutter create --template=package name_of_module
